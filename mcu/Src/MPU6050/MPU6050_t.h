5cf28f492c56856e47462367ba104f2b25919110
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2019-05-06 20:18:31 -0400

    Add STM32 project

---
+++ mcu/Src/MPU6050/MPU6050_t.h
@@ -0,0 +1,28 @@
+/**
+ * @file MPU6050_t.h
+ * @author Tyler
+ *
+ * @ingroup MPU6050_Driver
+ */
+
+#ifndef MPU6050_T_H_
+#define MPU6050_T_H_
+
+
+
+
+/*********************************** Types ************************************/
+/**
+ * @brief The data structure which stores the data read from the MPU6050 sensor
+ */
+typedef struct{
+	I2C_HandleTypeDef* hi2c; /**< I2C for this instance  */
+    float az; /**< Acceleration along z-axis             */
+    float ay; /**< Acceleration along y-axis             */
+    float ax; /**< Acceleration along x-axis             */
+    float vz; /**< Yaw rate (about z-axis)               */
+    float vy; /**< Pitch rate (about y-axis)             */
+    float vx; /**< Roll rate (about x-axis)              */
+}MPU6050_t;
+
+#endif /* MPU6050_T_H_ */
