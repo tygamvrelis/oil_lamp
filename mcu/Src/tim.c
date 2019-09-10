a964eb03b2b9caf399b757c73af83f280801a1d2
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2019-09-09 21:14:49 -0400

    Remove unecessary #include + update PWM settings
    PWM is now 100 Hz which makes more sense. This doesn't seem to impact control, but I think it's a better decision.

--- mcu/Src/tim.c
+++ mcu/Src/tim.c
@@ -34,8 +34,8 @@ void MX_TIM2_Init(void)
 
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 0;
-  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 225000;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_DOWN;
+  htim2.Init.Period = 900000;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
